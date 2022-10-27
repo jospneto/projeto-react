96cc892ae4833ef4df0afb778104c6e0ae0f6141
jospneto <josenetopereira380@gmail.com>
2022-10-27 08:20:21 -0300

    Atualização da página

--- proje_teste/src/App.js
+++ proje_teste/src/App.js
@@ -1,33 +1,53 @@
-import logo from './logo.svg';
-import perfil from './img/eu.png';
-import pb from './img/pb.png'
 import './App.css';
+import foto from './img/eu.png';
+import ciencia from './img/icons1.png';
+import fut from './img/icons3.png';
+import tec from './img/icons2.png';
+import pb from './img/pb.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a href="https://reactjs.org/docs/getting-started.html"><button type="submit">Clique aqui!</button></a>
-      </header>
-      <body className="App-body">
-        <h1>Um pouco sobre mim!</h1>
-        <div id="person">
-          <img src={perfil} alt="eu"/>
-          <div id="card">
-            <p>Olá me chamo José Pereira de Souza Neto, tenho 20 anos, sou paraibano sim senhor,
-              aluno do Instituto Federal do Sertão Pernambucano no campus Salgueiro, amo esportes e animes
-              estou iniciando minha carreira como desenvolvedor front e estou muito animado com a oportunidade de estagiar
-              na SDConecta. Vamos para cima!
-            </p>
-            <ul>
-              <li><img src={pb}/></li>
+    <div className="App-bd">
+      <nav className="App-nav">
+        <h1>José Neto</h1>
+        <ul>
+          <a><li>Home</li></a>
+          <a><li>Projetos</li></a>
+          <a><li>Mídias Sociais</li></a>
+          <a><li></li></a>
+        </ul>
+        <a href=""><button>Fale comigo</button></a>
+      </nav>
+      <hr/>
+      <div className="App-body">
+        <div className="body">
+          <div id="infos">
+            <h1>Olá meu nome é José Neto!</h1>
+            <h2>20 anos</h2>
+            <div id="regiao">
+              <img src={pb} alt="pb"/>
+              <h2>Paraibano</h2>
+            </div>
+            <ul id="hobies">
+              <div>
+                <img src={fut}/>
+                <li>Futebol</li>
+              </div>
+              <div>
+                <img src={ciencia}/>
+                <li>Ciência</li>
+              </div>
+              <div>
+                <img src={tec}/>
+                <li>Tecnologia</li>
+              </div>
             </ul>
           </div>
         </div>
-      </body>
-      <footer className="App-footer">
-      </footer>
+        <div id="user_image">
+          <img src={foto} alt="perfil"/>
+        </div>
+      </div>
     </div>
   );
 }
