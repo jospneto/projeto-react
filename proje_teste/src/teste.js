244b84286ec6d5d7a094adce7865437863c6f184
jospneto <josenetopereira380@gmail.com>
2022-11-01 08:52:15 -0300

    atualização do teste

--- proje_teste/src/teste.js
+++ proje_teste/src/teste.js
@@ -1,9 +1,17 @@
 const teste = () => {
     return(
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 35d343d (Envio do teste para a main)
         <div>
             <h1>Teste</h1>
             <h2>Teste 2</h2>
         </div>
+<<<<<<< HEAD
+=======
+>>>>>>> develop
+>>>>>>> 35d343d (Envio do teste para a main)
     )
 }
 
