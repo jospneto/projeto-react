1785aa3f459a07d105d6b3fe4e39d2afa4ae604f
jospneto <josenetopereira380@gmail.com>
2022-11-01 10:24:19 -0300

    solução de conflitos

--- proje_teste/src/teste.js
+++ proje_teste/src/teste.js
@@ -1,11 +1,6 @@
 const teste = () => {
     return(
-<<<<<<< HEAD
-        <div>
-            <h1>Teste</h1>
-            <h2>Teste 2</h2>
-        </div>
->>>>>>> develop
+        <h1>Teste</h1>
     )
 }
 
